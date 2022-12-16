6cefb2a679723c18041a65a55395897c2b6032ff
NicolaPlacentino <nyko0921@gmail.com>
2022-12-16 16:56:16 +0100

    completed exercise

--- script.js
+++ script.js
@@ -15,3 +15,43 @@ In ogni cella, deve comparire il numero corrispondente, in ordine da 1 a 100;
 #MILESTONE 4
 Al click sulla cella, stampiamo il numero della cella cliccata in console,
 poi coloriamo la cella d'azzurro!*/
+
+const grid = document.getElementById('grid')
+const button = document.getElementById('button')
+const playMessage = document.getElementById('play-message')
+
+const rows = 10
+const columns = 10
+
+
+function generateCell(content) {
+    const cell = document.createElement('div')
+    cell.classList.add('cell')
+    cell.append(content)
+
+    return cell
+}
+
+function appendCell(content) {
+    grid.appendChild(content)
+}
+
+button.addEventListener('click', function (){
+    playMessage.classList.add('d-none')
+    grid.classList.remove('d-none')
+
+    grid.innerHTML = ''
+    for (let i = 0; i < rows*columns; i++){
+
+        const cell = generateCell(i + 1)
+        
+        cell.addEventListener('click', function(){
+            cell.classList.toggle('clicked')
+            console.log(i + 1)
+        })
+
+        appendCell(cell)
+        
+
+    }
+})
